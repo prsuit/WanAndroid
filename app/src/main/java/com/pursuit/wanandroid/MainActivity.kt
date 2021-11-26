ddcb4d17f9037e08e086cf22b2f574835b1ce47b
prsuit <318663050@qq.com>
2021-11-26 23:59:06 +0800

    add splashscreen

--- app/src/main/java/com/pursuit/wanandroid/MainActivity.kt
+++ app/src/main/java/com/pursuit/wanandroid/MainActivity.kt
@@ -2,12 +2,14 @@ package com.pursuit.wanandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.layout_title_bar.view.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
         titleBar.title.text = "123"
         tv_txt1.apply {
